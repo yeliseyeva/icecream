a99382025f3e333f4bccf49e8b79da26963504c5
Ivan Laver <74679323+Ereyzer@users.noreply.github.com>
2021-02-20 19:32:18 +0200

    Updates

--- menu.234110a1.js
+++ menu.234110a1.js
@@ -156,7 +156,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64756" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64109" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
