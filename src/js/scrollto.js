886c5cf01737184870f4547039d1e905e9ab5efa
Mikheenko Dmitry Sergeevich <mikheenko.official@gmail.com>
2021-02-23 19:04:05 +0200

    Добавляет изменения в Arrow-up

--- src/js/scrollto.js
+++ src/js/scrollto.js
@@ -17,39 +17,11 @@ for (let anchor of anchors) {
   });
 }
 // Возврат наверх
-jQuery(document).ready(function () {
-  var btn = $('#button');
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
-    } else {
-      btn.removeClass('show');
-    }
-  });
-  btn.on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: 0 }, '300');
-  });
-});
 var btn = $('#button');
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 300) {
+$(window).on(function () {
+  if ($(window).scrollTop() < 300) {
     btn.addClass('show');
   } else {
     btn.removeClass('show');
   }
-});
-if ($(window).scrollTop() > 300) {
-  btn.addClass('show');
-} else {
-  btn.removeClass('show');
-}
-if ($(window).scrollTop() > 300) {
-  btn.addClass('show');
-} else {
-  btn.removeClass('show');
-}
-btn.on('click', function (e) {
-  e.preventDefault();
-  $('html, body').animate({ scrollTop: 0 }, '300');
 });
